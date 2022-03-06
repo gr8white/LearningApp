40b6c87f0271cffc48e8bac5faab125511e8d054
Derrick White <derrick.white@umbrage.com>
2022-03-06 15:57:31 -0600

    - Organize file structure - Add content model environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Derrick White on 3/6/22.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+}
--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  LearningApp
 //
 //  Created by Derrick White on 3/6/22.
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
