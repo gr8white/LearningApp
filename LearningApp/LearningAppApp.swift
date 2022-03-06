a074c5efe90ef0cce63c9f2aaf33547e556f3b86
Derrick White <derrick.white@umbrage.com>
2022-03-06 15:49:50 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Derrick White on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
